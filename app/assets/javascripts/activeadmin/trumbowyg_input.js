b0c6088cc7834c083db27ede7d76888824aff76d
mat@blocknot.es <mat@blocknot.es>
2018-11-04 08:45:14 +0100

    Fix: improved init for nested resources

--- app/assets/javascripts/activeadmin/trumbowyg_input.js
+++ app/assets/javascripts/activeadmin/trumbowyg_input.js
@@ -1,9 +1,20 @@
-$(document).ready( function() {
+function initTrumbowygEditors () {
   $('.trumbowyg-textarea').each(function () {
-    var options = {
-      svgPath: '/assets/active_admin-trumbowyg.svg'
-    };
-    options = $.extend({}, options, $(this).data( 'options' ));
-    $(this).trumbowyg( options );
+    if (!$(this).hasClass('trumbowyg-textarea--active')) {
+      var options = {
+        svgPath: '/assets/active_admin-trumbowyg.svg'
+      };
+      options = $.extend({}, options, $(this).data('options'));
+      $(this).trumbowyg(options);
+      $(this).addClass('trumbowyg-textarea--active');
+    }
   });
+}
+
+$(document).on('has_many_add:after', function () {
+  initTrumbowygEditors();
+});
+
+$(document).ready( function() {
+  initTrumbowygEditors();
 });
